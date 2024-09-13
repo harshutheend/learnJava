35a4425dccdce82097fabe46acd0ac45c1117706
Harsh Sinha <harshutheend@gmail.com>
2024-09-13 07:19:34 +0530

    new sum program

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static int sum(int num1, int num2){
+        int sum= num1+num2;
+        return sum;
+    }
+    public static void main(String args[]){
+        System.err.println(sum(5,6));
+    }
+    
+}
